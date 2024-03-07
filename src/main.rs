c39202b8838f08fad4867b26da5497a16b141475
Guilherme Moitinho <guilhermemoitinho3165@gmail.com>
2024-03-07 16:31:33 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+mod schema;
+mod model;
+mod services;
+
+use actix_web::{
+    web,
+    App,
+    HttpServer,
+    middleware::Logger
+};
+
+use dotenv::dotenv;
+
+use sqlx::{
+    Postgres,
+    Pool,
+    postgres::PgPoolOptions
+};
+
+pub struct AppState {
+    db: Pool<Postgres>
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server started successfully");
+
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info")
+    }
+
+    dotenv().ok();
+    env_logger::init();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must to be set");
+    let pool = match PgPoolOptions::new().max_connections(10).connect(&database_url).await {
+        Ok(pool) => {
+            println!("Connetion DB resolved");
+            pool
+        }
+        Err(error) => {
+            println!("Failed to connect to the dabase: {:?}", error);
+            std::process::exit(1)
+        }
+    };
+
+
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(AppState { db: pool.clone()}))
+            .configure(services::config)
+            .wrap(Logger::default())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run().await
+}
